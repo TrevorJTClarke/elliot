4e3b757bdca55dd35bb689d8fddee5ebdf716711
Trevor Clarke <besnoid@gmail.com>
2015-08-16 15:40:11 -0700

    setting up simple routes for devices ☕️

---
+++ modules/session.js
@@ -0,0 +1,11 @@
+/**
+ * Session
+ * Handles all requests, verify we have IoT only devices
+ */
+var Session = function(req, res, next) {
+    // TODO: finish some logic, woo!
+    console.log('Something is happening.');
+    next();
+};
+
+module.exports = Session;
--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
+var mongoose   = require('mongoose');
+var config     = require('./config/config.json');
+var Session    = require('./modules/session');
+var Devices    = require('./modules/devices');
+var Endpoints  = require('./modules/endpoints');
 
+// setup the DB connection
+mongoose.connect('mongodb://' + config.mongo.username + ':' + config.mongo.password + config.mongo.instance);
+
+// setup the middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// base server setup
 var port = process.env.PORT || 1337;
 var router = express.Router();
 
-// test route
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! it works!' });
-});
+// use elliot middleware
+router.use(Session);
 
-// test router handler
-app.use('/api', router);
+// main router handler
+app.use('/api', Endpoints);
+
+// catchall
+app.use('*',function(req, res) {
+  res.status(404).send('404');
+});
 
 // =============================================================================
 // START THE SERVER
